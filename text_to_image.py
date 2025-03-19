b74e1b549e412b4c67a297cae375c7f18347c856
Mark <mark@remarkablemark.org>
2025-03-18 22:10:28 -0400

    feat: add text_to_image.py

---
+++ text_to_image.py
@@ -0,0 +1,31 @@
+from diffusers import AutoPipelineForText2Image
+from random import randint
+from sys import maxsize
+from time import time
+import torch
+
+if torch.cuda.is_available():
+    device = "cuda"
+# TODO: RuntimeError: MPS backend out of memory
+# elif torch.backends.mps.is_available():
+#     device = "mps"
+else:
+    device = "cpu"
+
+# https://huggingface.co/docs/diffusers/main/en/tutorials/autopipeline?autopipeline=text-to-image
+model = "dreamlike-art/dreamlike-photoreal-2.0"
+pipe = AutoPipelineForText2Image.from_pretrained(
+    model,
+    torch_dtype=None if device == "cpu" else torch.float16,
+    use_safetensors=True
+)
+pipe.to(device)
+
+seed = randint(-maxsize, maxsize * 2)
+generator = torch.Generator(device=device).manual_seed(seed)
+prompt = "cinematic photo of Godzilla eating sushi with a cat in a izakaya, 35mm photograph, film, professional, 4k, highly detailed"
+image = pipe(prompt, generator=generator).images[0]
+
+filename = f"text-to-image{seed}.png"
+image.save(filename)
+print(f"Output: {filename}")
