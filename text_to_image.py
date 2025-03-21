0b05f300641894824de657e774b601c6b2e0eece
Mark <mark@remarkablemark.org>
2025-03-21 00:21:14 -0400

    refactor: tidy text_to_image.py

--- text_to_image.py
+++ text_to_image.py
@@ -1,30 +1,13 @@
-from random import randint
-from sys import maxsize
-import diffusers
-import torch
-
-if torch.cuda.is_available():
-    device = "cuda"
-# TODO: RuntimeError: MPS backend out of memory
-# elif torch.backends.mps.is_available():
-#     device = "mps"
-else:
-    device = "cpu"
+from diffusers import AutoPipelineForText2Image
+from uuid import uuid1
 
 # https://huggingface.co/docs/diffusers/main/en/tutorials/autopipeline?autopipeline=text-to-image
 model = "dreamlike-art/dreamlike-photoreal-2.0"
-pipeline = diffusers.AutoPipelineForText2Image.from_pretrained(
-    model,
-    torch_dtype=None if device == "cpu" else torch.float16,
-    use_safetensors=True
-)
-pipeline.to(device)
+pipeline = AutoPipelineForText2Image.from_pretrained(model)
 
 prompt = "cinematic photo of Godzilla eating sushi with a cat in a izakaya, 35mm photograph, film, professional, 4k, highly detailed"
-seed = randint(-maxsize, maxsize * 2)
-generator = torch.Generator(device=device).manual_seed(seed)
-image = pipeline(prompt, generator=generator).images[0]
+image = pipeline(prompt).images[0]
 
-filename = f"text-to-image{seed}.png"
+filename = f"text-to-image-{uuid1()}.png"
 image.save(filename)
 print(f"Output: {filename}")
