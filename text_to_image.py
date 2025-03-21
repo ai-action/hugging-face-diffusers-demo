eb6ab89894b8f05fdc0aa3f5f7185238e8eb33e2
Mark <mark@remarkablemark.org>
2025-03-20 23:13:47 -0400

    refactor: tidy imports in text_to_image.py

--- text_to_image.py
+++ text_to_image.py
@@ -1,6 +1,6 @@
-from diffusers import AutoPipelineForText2Image
 from random import randint
 from sys import maxsize
+import diffusers
 import torch
 
 if torch.cuda.is_available():
@@ -13,17 +13,17 @@ else:
 
 # https://huggingface.co/docs/diffusers/main/en/tutorials/autopipeline?autopipeline=text-to-image
 model = "dreamlike-art/dreamlike-photoreal-2.0"
-pipe = AutoPipelineForText2Image.from_pretrained(
+pipeline = diffusers.AutoPipelineForText2Image.from_pretrained(
     model,
     torch_dtype=None if device == "cpu" else torch.float16,
     use_safetensors=True
 )
-pipe.to(device)
+pipeline.to(device)
 
+prompt = "cinematic photo of Godzilla eating sushi with a cat in a izakaya, 35mm photograph, film, professional, 4k, highly detailed"
 seed = randint(-maxsize, maxsize * 2)
 generator = torch.Generator(device=device).manual_seed(seed)
-prompt = "cinematic photo of Godzilla eating sushi with a cat in a izakaya, 35mm photograph, film, professional, 4k, highly detailed"
-image = pipe(prompt, generator=generator).images[0]
+image = pipeline(prompt, generator=generator).images[0]
 
 filename = f"text-to-image{seed}.png"
 image.save(filename)
